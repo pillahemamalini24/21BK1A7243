c0ce61b6c1fe43e8c45ec80b16dd7b74119be675
pillahemamalini24 <166357763+pillahemamalini24@users.noreply.github.com>
2024-07-15 12:22:25 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AverageCalculator from './components/average_Calculator'
+import AverageCalculator from './components/average_calculator'
 
 function App() {
   const [count, setCount] = useState(0)
